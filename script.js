c8dca37625d920b7c6544952b27fb3684885adab
shazidhasanriam <shazid.riam@northsouth.edu>
2021-12-04 09:57:17 +0600

    Save Bookamrk functionality Added

--- script.js
+++ script.js
@@ -40,6 +40,61 @@ function validate(nameValue, urlValue) {
   // Valid
   return true;
 }
+
+// Building Bookmarks DOM
+function buildBookmarks() {
+  // Building Items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // console.log(name, url);
+
+    // Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    // Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    //Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    //Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch bookmarks from local storage
+function fetchBookmarks() {
+  // Get bookmarks from local storage if any
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in local storage
+    bookmarks = [
+      {
+        name: "Riam",
+        url: "riam.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
 // Manage Data from form input
 function storeBookmark(e) {
   e.preventDefault();
@@ -57,10 +112,15 @@ function storeBookmark(e) {
     url: urlValue,
   };
   bookmarks.push(bookmark);
-  console.log(bookmarks);
+  // console.log(JSON.stringify(bookmarks));
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
   bookmarkForm.reset();
   websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On Load Fetching Bookmarks
+fetchBookmarks();
